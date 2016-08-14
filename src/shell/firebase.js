06fdd767f81208a2bc3033a08cbd24e299098ae9
Sebastian Reinhard <sebastian@sebbert.me>
2016-08-14 19:14:06 +0200

    FUCKFUCK

--- src/shell/firebase.js
+++
@@ -1,4 +0,0 @@
-const Firebase = require("firebase");
-const BASE_URL = "https://crowdplayspace.firebaseio.com";
-
-module.exports = (node) => new Firebase(BASE_URL + (node[0] == "/" ? "" : "/") + node);
