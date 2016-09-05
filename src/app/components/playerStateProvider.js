b75becb34812f2a632932fc005addd19d64d6ba0
Sebastian Reinhard <sebastian@sebbert.me>
2016-09-05 10:47:42 +0200

    hahaha types

--- src/app/components/playerStateProvider.js
+++ src/app/components/playerStateProvider.js
@@ -1,3 +1,4 @@
 import createProvider from './provider';
+import { PropTypes } from 'react';
 
-export default createProvider("playerState");
+export default createProvider("playerState", PropTypes.object);
