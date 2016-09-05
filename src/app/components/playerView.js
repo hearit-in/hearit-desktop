290906bfc55466e311def20a6fcd76b254805675
Sebastian Reinhard <sebastian@sebbert.me>
2016-09-05 10:48:05 +0200

    Prompt user to disable repeat when it's enabled

--- src/app/components/playerView.js
+++ src/app/components/playerView.js
@@ -2,12 +2,10 @@ import React, {PropTypes} from 'react';
 
 import { on, ready as playerReady } from '../ipc/playerInterface';
 import { Col, Row } from './layout';
-
 import { firebaseForRoomId } from '../firebase';
-
 import controllerProvider from './controllerProvider';
-
 import QueueController from '../firebase/queueController';
+import * as player from '../ipc/playerInterface';
 
 import {
 	Card,
@@ -37,6 +35,7 @@ export default class PlayerView extends React.Component {
 
 		this.state = {
 			track: EmptyTrack,
+			playerStatus: undefined,
 			shouldPlay: true
 		};
 	}
@@ -53,12 +52,20 @@ export default class PlayerView extends React.Component {
 		this.ref.on("value", (snapshot) => {
 			this.setTrack(snapshot.val());
 		});
+		
+		this.onPlayerStatus = player.on("status", (event, response) => {
+			console.log(response);
+			this.setState({ playerStatus: response.status });
+		});
 	}
 
 	componentWillUnmount() {
 		this.ref.off();
+		
 		this.queueController.unlisten();
 		delete this.queueController;
+		
+		player.off("status", this.onPlayerStatus);
 	}
 
 	setTrack(track) {
@@ -87,26 +94,37 @@ export default class PlayerView extends React.Component {
 			display: "block",
 			margin: "0px auto"
 		};
+		
+		console.dir(this.state.playerStatus);
+		
+		let isOnRepeat = this.state.playerStatus && this.state.playerStatus.repeat;
 
 		return (
-			<div className="player-container">
-				<div className="player" >
-					<img src={track.images.large} className="player-image" />
+			<div>
+				<div className="repeat-warning"
+					style={{
+						opacity: isOnRepeat ? 1 : 0
+					}}>
+				</div>
+				<div className="player-container">
+					<div className="player" >
+						<img src={track.images.large} className="player-image" />
 
-					<div className="player-right">
-						<div className="player-info">
-							<span className="player-info-name">{track.name}</span>
-							<span className="player-info-artist">{track.artist}</span>
+						<div className="player-right">
+							<div className="player-info">
+								<span className="player-info-name">{track.name}</span>
+								<span className="player-info-artist">{track.artist}</span>
+							</div>
 						</div>
 					</div>
-				</div>
-				
-				<div className="player-play-button" onClick={() => this.toggleShouldPlay()}>
-					<iconClass className="player-play-button-icon"  />
-					{ this.state.shouldPlay
-						? <AvPause style={iconStyle} />
-						: <AvPlayArrow style={iconStyle} />
-					}
+					
+					<div className="player-play-button" onClick={() => this.toggleShouldPlay()}>
+						<iconClass className="player-play-button-icon"  />
+						{ this.state.shouldPlay
+							? <AvPause style={iconStyle} />
+							: <AvPlayArrow style={iconStyle} />
+						}
+					</div>
 				</div>
 			</div>
 		);
@@ -115,7 +133,8 @@ export default class PlayerView extends React.Component {
 
 PlayerView.contextTypes = {
 	roomId: PropTypes.string,
-	controller: PropTypes.object
+	controller: PropTypes.object,
+	playerState: PropTypes.object
 };
 
 PlayerView.propTypes = {
